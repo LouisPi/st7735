a28f76c591f3607af331e2d4985736218870e130
LouisPi <granbobbi22@gmail.com>
2018-11-09 22:11:02 +0000

    Update st7735.py

--- st7735.py
+++ st7735.py
@@ -4,5 +4,6 @@ from luma.lcd.device import st7735
 from output.output import OutputDevice
 
 class Screen(LumaScreen, OutputDevice):
+  device_mode = "RGB"
   def init_display(self, autoscroll=False, **kwargs):
       self.device = st7735(self.serial, width=128, height=128, bgr=True, h_offset=1, v_offset=2)
